4219630ef5b9b9f0850459496a126480785e46c9
Koza <kiskoza@gmail.com>
2023-07-19 09:39:12 +0200

    Support more login forms

--- src/containers.js
+++ src/containers.js
@@ -1,13 +1,18 @@
+SIGN_IN_PATHS = ['/sign_in', '/users/sign_in'];
+FORM = SIGN_IN_PATHS.map(el => `form[action="${el}"]`).join(', ');
+USERNAME_FIELD = ['text', 'email'].map(el => `input[type=${el}]`).join(', ');
+PASSWORD_FIELD = ['password'].map(el => `input[type=${el}]`).join(', ');
+
 (async function init () {
   browser.runtime.onMessage.addListener(message => {
     switch (message.action) {
     case 'ping':
       return 'pong';
     case "login":
-      let signInForm = document.querySelector('form[action="/sign_in"]');
+      let signInForm = document.querySelector(FORM);
 
-      signInForm.querySelector('input[type=text]').value = message.email;
-      signInForm.querySelector('input[type=password]').value = message.password;
+      signInForm.querySelector(USERNAME_FIELD).value = message.email;
+      signInForm.querySelector(PASSWORD_FIELD).value = message.password;
 
       signInForm.submit();
 
@@ -17,11 +22,11 @@
     }
   });
 
-  if (window.location.pathname === '/sign_in') {
-    let signInForm = document.querySelector('form[action="/sign_in"]')
+  if (SIGN_IN_PATHS.includes(window.location.pathname)) {
+    let signInForm = document.querySelector(FORM)
     signInForm.addEventListener('submit', async (event) => {
-      let email = event.target.querySelector('input[type=text]').value;
-      let password = event.target.querySelector('input[type=password]').value;
+      let email = event.target.querySelector(USERNAME_FIELD).value;
+      let password = event.target.querySelector(PASSWORD_FIELD).value;
 
       await browser.runtime.sendMessage({
         message: "login",
